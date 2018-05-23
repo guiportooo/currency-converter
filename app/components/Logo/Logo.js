e2c0d7a909485e13c4dcfd8b19a927a3eb862c5d
Guilherme Porto <guiportooo@gmail.com>
2018-05-23 01:00:37 -0300

    Adding Home Screen Logo

---
+++ app/components/Logo/Logo.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { View, Image, ImageBackground, Text } from 'react-native';
+import styles from './styles';
+
+const Logo = () => (
+  <View style={styles.container}>
+    <ImageBackground
+      style={styles.containerBackground}
+      imageStyle={styles.containerImage}
+      source={require('./images/background.png')}
+    >
+      <Image style={styles.image} source={require('./images/logo.png')} />
+    </ImageBackground>
+    <Text style={styles.text}>Currency Converter</Text>
+  </View>
+);
+
+export default Logo;
+
---
+++ app/components/Logo/styles.js
@@ -0,0 +1,30 @@
+import { Dimensions } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+const imageWidth = Dimensions.get('window').width / 2;
+
+export default EStyleSheet.create({
+  container: {
+    alignItems: 'center',
+  },
+  containerBackground: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: imageWidth,
+    height: imageWidth,
+  },
+  containerImage: {
+    resizeMode: 'contain',
+  },
+  image: {
+    width: imageWidth / 2,
+    resizeMode: 'contain',
+  },
+  text: {
+    fontWeight: '600',
+    fontSize: 28,
+    letterSpacing: -0.5,
+    marginTop: 15,
+    color: '$white',
+  },
+});
